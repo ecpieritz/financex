f4eb679c45b23ab0c659d0b65f87ff051de7ada8
unknown <ecpieritz@gmail.com>
2023-10-20 13:39:06 -0300

    Added col 1 of banner

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import HeroBanner from "./components/HeroBanner";
 import Navbar from "./components/Navbar";
 import "./scss/index.scss";
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <HeroBanner />
     </div>
   );
 }
---
+++ src/components/HeroBanner.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function HeroBanner() {
+  return (
+    <div className='fx-banner'>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-6">
+            <h4 className='fx-text green'>Welcome! Future Platform</h4>
+            <h1>The World's Most Modern Card <span className='fx-text green'>Platform</span></h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor</p>
+            <a className='fx-btn' href="#about">Get Started</a>
+          </div>
+          <div className="col-6">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeroBanner
