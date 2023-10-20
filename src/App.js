01063e1d7b12c0c8947d75a48820cb6d28e612b4
unknown <ecpieritz@gmail.com>
2023-10-20 11:16:08 -0300

    Added projects base

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Accordion } from "react-bootstrap";
+import "./scss/index.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-primary">App working</h1>
+    </div>
+  );
+}
+
+export default App;
