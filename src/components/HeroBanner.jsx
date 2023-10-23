efd94123ba4524fc059ece2c1de5a2f0ee2eb72b
omzanalytics <epieritz@omz.ag>
2023-10-23 10:29:51 -0300

    Changed index order

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import img from '../assets/img/fx-banner__img.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons'
 
 function HeroBanner() {
   return (
@@ -10,7 +12,10 @@ function HeroBanner() {
             <h4 className='fx-text green'>Welcome! Future Platform</h4>
             <h1>The World's Most Modern Card <span className='fx-text green'>Platform</span></h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor</p>
-            <a className='fx-btn' href="#about">Get Started</a>
+            <a className='fx-btn' href="#about">
+              Get Started
+              <FontAwesomeIcon icon={faArrowRightLong} />
+            </a>
           </div>
           <div className="col-6">
             <img className='fx-banner__img' src={img} alt="credit card with green circle" />
