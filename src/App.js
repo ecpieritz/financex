47030c9e8425b8f4676ea59413cf31aeefeddc53
ecpieritz <ecpieritz@gmail.com>
2023-10-24 15:31:10 -0300

    Added Contact base

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from "./components/About";
 import Benefits from "./components/Benefits";
 import Services from "./components/Services";
 import Customers from "./components/Customers";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Benefits />
         <Services />
         <Customers />
+        <Contact />
       </main>
     </div>
   );
---
+++ src/components/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <section className="fx-contact" id='contact'>
+      <div className="container-fluid">
+        <h4>Our Costumers</h4>
+        <h2>Let’s grow your business, together!</h2>  
+
+      </div>
+    </section>
+  )
+}
+
+export default Contact
