8eea313756915a65229dfed6287f51540568652e
ecpieritz <ecpieritz@gmail.com>
2023-10-24 11:24:42 -0300

    Added infos part of Services Section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRightLong } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRightLong, faLocationDot, faGlobe, faSignal } from "@fortawesome/free-solid-svg-icons";
 import ServiceImg from '../assets/img/fx-service__img.png'
 
 function Services() {
@@ -25,6 +25,27 @@ function Services() {
           </div>
           <div className="col-md-6">
             <img src={ServiceImg} alt="Visa card over a green circle" />
+          </div>
+        </div>
+
+        <div className="row fx-services__infos">
+          <div className="col-lg-4">
+            <FontAwesomeIcon icon={faLocationDot} />
+            <h3>Local business finance</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut  </p>
+            <a className="fx-btn text-btn" href="#!"> Learn more <FontAwesomeIcon icon={faArrowRightLong} /></a>
+          </div>
+          <div className="col-lg-4">
+            <FontAwesomeIcon icon={faGlobe} />
+            <h3>Built for global payments</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut  </p>
+            <a className="fx-btn text-btn" href="#!"> Learn more <FontAwesomeIcon icon={faArrowRightLong} /></a>
+          </div>
+          <div className="col-lg-4">
+            <FontAwesomeIcon icon={faSignal} />
+            <h3>Make internet of Money</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut  </p>
+            <a className="fx-btn text-btn" href="#!"> Learn more <FontAwesomeIcon icon={faArrowRightLong} /></a>
           </div>
         </div>
       </div>
