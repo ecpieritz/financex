3610acd8b721f9a2749ff34f9ffed9b22374082f
ecpieritz <ecpieritz@gmail.com>
2023-10-24 09:56:59 -0300

    Added Services section base

---
+++ src/components/Services.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Services() {
+  return (
+    <section className='fx-services'>
+      <div className="container-fluid">
+        <div className="col-md-6"></div>
+        <div className="col-md-6"></div>
+      </div>
+    </section>
+  )
+}
+
+export default Services
